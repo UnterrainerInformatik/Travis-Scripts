6bf7712935136367fa25dbd7e796344eb4c9d46d
Gerald Unterrainer <gu_falcon@hotmail.com>
2020-01-14 23:09:47 +0100

    manual ms install

--- .NET/scripts/inst_dotnetcoresdk.sh
+++ .NET/scripts/inst_dotnetcoresdk.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
-echo "getting MS setup"
-wget -q https://packages.microsoft.com/config/ubuntu/16.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
-echo "installing MS setup"
-sudo dpkg -i packages-microsoft-prod.deb
-
-echo "update"
+sudo apt-get install -y gpg
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor -o microsoft.asc.gpg
+sudo mv microsoft.asc.gpg /etc/apt/trusted.gpg.d/
+wget -q https://packages.microsoft.com/config/ubuntu/16.04/prod.list
+sudo mv prod.list /etc/apt/sources.list.d/microsoft-prod.list
+sudo chown root:root /etc/apt/trusted.gpg.d/microsoft.asc.gpg
+sudo chown root:root /etc/apt/sources.list.d/microsoft-prod.list
+sudo apt-get install -y apt-transport-https
 sudo apt-get update
-echo "install apt-transport-https"
-sudo apt-get install apt-transport-https
-echo "update"
-sudo apt-get update
-echo "install dotnet-sdk-3.1"
 sudo apt-get install dotnet-sdk-3.1
