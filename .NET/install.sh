ef21d36b692bcd5ce9d58c0576655e8d96d72641
Gerald Unterrainer <gu_falcon@hotmail.com>
2020-01-14 17:26:29 +0100

    Fix

--- .NET/install.sh
+++ .NET/install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 nuget restore $SOLUTION_NAME.sln
 
-$(tr_getWorkDir)/script/inst_netCoresdk.sh
-$(tr_getWorkDir)/script/inst_minver.sh
+$(tr_getWorkDir)/.NET/script/inst_netCoresdk.sh
+$(tr_getWorkDir)/.NET/script/inst_minver.sh
