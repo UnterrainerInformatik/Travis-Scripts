0390d15659ee325af4df79af513552a7d5c1f681
Gerald Unterrainer <gu_falcon@hotmail.com>
2020-01-14 22:59:58 +0100

    add debugging

--- .NET/scripts/inst_dotnetcoresdk.sh
+++ .NET/scripts/inst_dotnetcoresdk.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
+echo "getting MS setup"
 wget -q https://packages.microsoft.com/config/ubuntu/16.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
-sudo dpkg --purge packages-microsoft-prod
+echo "installing MS setup"
 sudo dpkg -i packages-microsoft-prod.deb
 
+echo "update"
 sudo apt-get update
+echo "install apt-transport-https"
 sudo apt-get install apt-transport-https
+echo "update"
 sudo apt-get update
+echo "install dotnet-sdk-3.1"
 sudo apt-get install dotnet-sdk-3.1
