fbc7763e9d62e18ff105c9a891fb4bf5359d70a4
Gerald Unterrainer <gu_falcon@hotmail.com>
2020-01-14 16:44:49 +0100

    Try with workdir

--- .NET/install.sh
+++ .NET/install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 nuget restore $SOLUTION_NAME.sln
 
-script/inst_netCoresdk.sh
-script/inst_minver.sh
+$(tr_getWorkDir)/script/inst_netCoresdk.sh
+$(tr_getWorkDir)/script/inst_minver.sh
--- functions.sh
+++ functions.sh
@@ -23,3 +23,15 @@ tr_isSetAndNotFalse() {
         fi
     fi
 }
+
+tr_getWorkDir() {
+    local SOURCE="${BASH_SOURCE[0]}"
+    local DIR
+    while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+        DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+        SOURCE="$(readlink "$SOURCE")"
+        [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+    done
+    DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+    return $DIR
+}
