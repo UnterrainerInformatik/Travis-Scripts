75dfe404e5c9e52a13e32359f8d128748b60c976
Gerald Unterrainer <gu_falcon@hotmail.com>
2020-01-14 18:35:36 +0100

    Simplify

--- .NET/install.sh
+++ .NET/install.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 nuget restore $SOLUTION_NAME.sln
 
-$TRAVIS/script/inst_netCoresdk.sh
-$TRAVIS/script/inst_minver.sh
+# Install .NET core SDK
+wget -q https://packages.microsoft.com/config/ubuntu/16.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
+
+sudo apt-get update
+sudo apt-get install apt-transport-https
+sudo apt-get update
+sudo apt-get install dotnet-sdk-3.1
+
+# Install minver core util
+dotnet tool install --global minver-cli --version 2.0.0
--- .NET/script/inst_minver.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-dotnet tool install --global minver-cli --version 2.0.0
