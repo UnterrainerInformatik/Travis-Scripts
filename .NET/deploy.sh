6a102cfcc557e183275ec9dd83c38c3184e692b3
Gerald Unterrainer <gu_falcon@hotmail.com>
2020-01-15 09:18:24 +0100

    Update

--- .NET/deploy.sh
+++ .NET/deploy.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-if tr_isSetAndNotFalse DEPLOY; then
-    nuget pack ${PROJECT_PATH}${PROJECT_FILENAME}.csproj -Version $VERSION -Verbosity detailed -Symbols -SymbolPackageFormat snupkg -Prop Configuration=$DEPLOY_BUILD
-    nuget push $PROJECT_FILENAME.*.nupkg -Verbosity detailed -ApiKey $NUGET_API_KEY -source https://www.nuget.org
+echo "starting deploy stage"
+if tr_isSetAndNotFalse NUGET; then
+    echo "NUGET is set"
+    nuget pack ${NUGET_PROJECT_PATH}${NUGET_PROJECT_FILENAME}.csproj -Version $VERSION -Verbosity detailed -Symbols -SymbolPackageFormat snupkg -Prop Configuration=$DEPLOY_BUILD
+    nuget push $NUGET_PROJECT_FILENAME.*.nupkg -Verbosity detailed -ApiKey $NUGET_API_KEY -source https://www.nuget.org
 else
-    echo "skipped deploy since env-variable DEPLOY is not set or set to [false]."
+    echo "skipped deploying nuget since env-variable NUGET is not set or set to [false]."
 fi
