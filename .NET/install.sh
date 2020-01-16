b50bbd0c08bf441fc46b2767c16d5ddd64551be4
Gerald Unterrainer <gu_falcon@hotmail.com>
2020-01-16 09:48:06 +0100

    Source every script

--- .NET/install.sh
+++ .NET/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 nuget restore $SOLUTION_NAME.sln
 
-$TRAVIS/scripts/inst_dotnetcoresdk.sh
-$TRAVIS/scripts/inst_minver.sh
-$TRAVIS/scripts/inst_monogame.sh
+source $TRAVIS/scripts/inst_dotnetcoresdk.sh
+source $TRAVIS/scripts/inst_minver.sh
+source $TRAVIS/scripts/inst_monogame.sh
