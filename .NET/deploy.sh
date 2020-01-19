fc68d15da504ba863288a0e68de8f67edd8db82a
Gerald Unterrainer <gu_falcon@hotmail.com>
2020-01-19 09:24:28 +0100

    Trying re-sourcing of fn in deploy stage in before script

--- .NET/deploy.sh
+++ .NET/deploy.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# Since the deploy-stage has limited bash capabilities AND is a different shell,
-# we have to re-import everyting.
-# Environment variables should be set though (travis takes care of that).
-echo "re-sourcing functions"
-source travis/functions.sh
-
 echo "starting deploy stage"
 if tr_isSetAndNotFalse NUGET; then
     echo "NUGET is set"
