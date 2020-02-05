de5c071225b36bc666cb1c6a1f400da855afcccb
Gerald Unterrainer <gu_falcon@hotmail.com>
2020-02-05 09:20:40 +0100

    Add skip-duplicate in nuget publish step

--- .NET/deploy.sh
+++ .NET/deploy.sh
@@ -10,7 +10,7 @@ echo "starting deploy stage"
 if tr_isSetAndNotFalse NUGET; then
     echo "NUGET is set"
     nuget pack ${NUGET_PROJECT_PATH}${NUGET_PROJECT_FILENAME}.csproj -Version $VERSION -Verbosity detailed -Symbols -SymbolPackageFormat snupkg -Prop Configuration=$DEPLOY_BUILD
-    nuget push $NUGET_PROJECT_FILENAME.*.nupkg -Verbosity detailed -ApiKey $NUGET_API_KEY -source https://www.nuget.org
+    nuget push $NUGET_PROJECT_FILENAME.*.nupkg -Verbosity detailed -ApiKey $NUGET_API_KEY -source https://www.nuget.org -SkipDuplicate
 else
     echo "skipped deploying nuget since env-variable NUGET is not set or set to [false]."
 fi
