08a0b98f18f5c487258137f76922db3dc2264d42
Gerald Unterrainer <gu_falcon@hotmail.com>
2020-09-08 00:36:15 +0200

    fix location of settings.xml

--- Java/script.sh
+++ Java/script.sh
@@ -11,4 +11,4 @@ export GROUP_ID=$(echo "$output" | grep '^GROUP_ID' | cut -d = -f 2)
 export ARTIFACT_ID=$(echo "$output" | grep '^ARTIFACT_ID' | cut -d = -f 2)
 export POM_VERSION=$(echo "$output" | grep '^POM_VERSION' | cut -d = -f 2)
 
-mvn clean deploy --settings ./$TRAVIS/settings.xml -DskipTests=true -B -U -Prelease
+mvn clean deploy --settings $TRAVIS/settings.xml -DskipTests=true -B -U -Prelease
