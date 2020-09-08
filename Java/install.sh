a64d79d4ff44779820150b76a9d6661d770bf5cc
Gerald <gu_falcon@hotmail.com>
2020-09-08 08:34:49 +0200

    Update install.sh

--- Java/install.sh
+++ Java/install.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 
-echo Importing gpg secret key
-echo $GPG_SECRET_KEYS | base64 --decode | $GPG_EXECUTABLE --import
-echo importing gpg ownertrust
-echo $GPG_OWNERTRUST | base64 --decode | $GPG_EXECUTABLE --import-ownertrust
-
 mvn --settings $TRAVIS/settings.xml install -DskipTests=true -Dgpg.skip -Dmaven.javadoc.skip=true -B -V
