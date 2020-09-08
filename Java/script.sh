46a21b86b42b316ab84f5abebda9620356b6ab22
Gerald <gu_falcon@hotmail.com>
2020-09-08 10:17:08 +0200

    Update script.sh

--- Java/script.sh
+++ Java/script.sh
@@ -1,25 +1,10 @@
 #!/bin/bash
-    
-export LOCAL_REPO=$(mvn -q \
-    -Dexec.executable=echo \
-    -Dexec.args='${settings.localRepository}' \
-    --non-recursive \
-    exec:exec)
-export GROUP_ID=$(mvn -q \
-    -Dexec.executable=echo \
-    -Dexec.args='${project.groupId}' \
-    --non-recursive \
-    exec:exec)
-export ARTIFACT_ID=$(mvn -q \
-    -Dexec.executable=echo \
-    -Dexec.args='ARTIFACT_ID=${project.artifactId}' \
-    --non-recursive \
-    exec:exec)
-export POM_VERSION=$(mvn -q \
-    -Dexec.executable=echo \
-    -Dexec.args='${project.version}' \
-    --non-recursive \
-    exec:exec)
+
+echo "Extracting project data from POM"
+export LOCAL_REPO=$(mvn -q -Dexec.executable=echo -Dexec.args='${settings.localRepository}' --non-recursive exec:exec)
+export GROUP_ID=$(mvn -q -Dexec.executable=echo -Dexec.args='${project.groupId}' --non-recursive exec:exec)
+export ARTIFACT_ID=$(mvn -q -Dexec.executable=echo -Dexec.args='${project.artifactId}' --non-recursive exec:exec)
+export POM_VERSION=$(mvn -q -Dexec.executable=echo -Dexec.args='${project.version}' --non-recursive exec:exec)
 
 echo "LOCAL_REPO=${LOCAL_REPO}"
 echo "GROUP_ID=${GROUP_ID}"
