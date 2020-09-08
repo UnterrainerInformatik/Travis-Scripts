baefb71c93511b8eb02f11165a3b7dfe063643f3
Gerald <gu_falcon@hotmail.com>
2020-09-08 09:01:41 +0200

    Update before_install.sh

--- Java/before_install.sh
+++ Java/before_install.sh
@@ -4,3 +4,6 @@ echo Importing gpg secret key
 echo $GPG_SECRET_KEYS | base64 --decode | $GPG_EXECUTABLE --import --no-tty --batch
 echo importing gpg ownertrust
 echo $GPG_OWNERTRUST | base64 --decode | $GPG_EXECUTABLE --import-ownertrust
+
+echo "allow-loopback-pinentry" >> ~/.gnupg/gpg-agent.conf
+gpgconf --reload gpg-agent
