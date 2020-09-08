2db93df6d079a33d4fa7dd9b5b1f833a4e899a52
Gerald <gu_falcon@hotmail.com>
2020-09-08 08:47:56 +0200

    Update before_install.sh

--- Java/before_install.sh
+++ Java/before_install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo Importing gpg secret key
-echo $GPG_SECRET_KEYS | base64 --decode | $GPG_EXECUTABLE --import
+echo $GPG_SECRET_KEYS | base64 --decode | $GPG_EXECUTABLE --import --no-tty --batch
 echo importing gpg ownertrust
 echo $GPG_OWNERTRUST | base64 --decode | $GPG_EXECUTABLE --import-ownertrust
