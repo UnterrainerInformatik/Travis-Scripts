a5b5834097999a5da71a1c7a3324e63039740baa
Psilo <psilo@unterrainer.info>
2020-09-15 13:44:44 +0200

    update

--- Java/before_deploy.sh
+++ Java/before_deploy.sh
@@ -75,6 +75,10 @@ fi
 
 if tr_isSetAndNotFalse DEPLOY; then
     echo "DEPLOY is set -> starting to prepare SSH-deployment-phase"
+    echo "DEPLOY is set -> importing SSH keys"
+    echo "openssl aes-256-cbc -K ${!DEPLOYMENT_SSH_KEY_VARNAME} -iv ${!DEPLOYMENT_SSH_IV_VARNAME} -in $SSH_ENC_FILE_NAME_WO_EXT.enc -out /tmp/$SSH_ENC_FILE_NAME_WO_EXT -d"
+    openssl aes-256-cbc -K ${!DEPLOYMENT_SSH_KEY_VARNAME} -iv ${!DEPLOYMENT_SSH_IV_VARNAME} -in $SSH_ENC_FILE_NAME_WO_EXT.enc -out /tmp/$SSH_ENC_FILE_NAME_WO_EXT -d
+
     echo "configuring SSH agent"
     eval "$(ssh-agent -s)"
     echo "chmod 600 /tmp/$SSH_ENC_FILE_NAME_WO_EXT"
