09d96c8c35d7584d33d7adc590f02a01592a9a9b
Psilo <psilo@unterrainer.info>
2020-09-15 13:52:11 +0200

    update

--- Java/before_deploy.sh
+++ Java/before_deploy.sh
@@ -72,18 +72,3 @@ if tr_isSetAndNotFalse DOCKER_REGISTRY; then
     docker push $BUILD_VER && docker image rm $BUILD_VER
     echo "done preparing docker-deployment-phase"
 fi
-
-if tr_isSetAndNotFalse DEPLOY; then
-    echo "DEPLOY is set -> starting to prepare SSH-deployment-phase"
-    echo "DEPLOY is set -> importing SSH keys"
-    echo "openssl aes-256-cbc -K ${!DEPLOYMENT_SSH_KEY_VARNAME} -iv ${!DEPLOYMENT_SSH_IV_VARNAME} -in $SSH_ENC_FILE_NAME_WO_EXT.enc -out /tmp/$SSH_ENC_FILE_NAME_WO_EXT -d"
-    openssl aes-256-cbc -K ${!DEPLOYMENT_SSH_KEY_VARNAME} -iv ${!DEPLOYMENT_SSH_IV_VARNAME} -in $SSH_ENC_FILE_NAME_WO_EXT.enc -out /tmp/$SSH_ENC_FILE_NAME_WO_EXT -d
-
-    echo "configuring SSH agent"
-    eval "$(ssh-agent -s)"
-    echo "chmod 600 /tmp/$SSH_ENC_FILE_NAME_WO_EXT"
-    chmod 600 /tmp/$SSH_ENC_FILE_NAME_WO_EXT
-    echo "ssh-add /tmp/$SSH_ENC_FILE_NAME_WO_EXT"
-    ssh-add /tmp/$SSH_ENC_FILE_NAME_WO_EXT
-    echo "done preparing SSH-deployment-phase"
-fi
--- Java/before_install.sh
+++ Java/before_install.sh
@@ -19,3 +19,18 @@ echo "LOCAL_REPO=${LOCAL_REPO}"
 echo "GROUP_ID=${GROUP_ID}"
 echo "ARTIFACT_ID=${ARTIFACT_ID}"
 echo "POM_VERSION=${POM_VERSION}"
+
+if tr_isSetAndNotFalse DEPLOY; then
+    echo "DEPLOY is set -> starting to prepare SSH-deployment-phase"
+    echo "DEPLOY is set -> importing SSH keys"
+    echo "openssl aes-256-cbc -K ${!DEPLOYMENT_SSH_KEY_VARNAME} -iv ${!DEPLOYMENT_SSH_IV_VARNAME} -in $SSH_ENC_FILE_NAME_WO_EXT.enc -out /tmp/$SSH_ENC_FILE_NAME_WO_EXT -d"
+    openssl aes-256-cbc -K ${!DEPLOYMENT_SSH_KEY_VARNAME} -iv ${!DEPLOYMENT_SSH_IV_VARNAME} -in $SSH_ENC_FILE_NAME_WO_EXT.enc -out /tmp/$SSH_ENC_FILE_NAME_WO_EXT -d
+
+    echo "configuring SSH agent"
+    eval "$(ssh-agent -s)"
+    echo "chmod 600 /tmp/$SSH_ENC_FILE_NAME_WO_EXT"
+    chmod 600 /tmp/$SSH_ENC_FILE_NAME_WO_EXT
+    echo "ssh-add /tmp/$SSH_ENC_FILE_NAME_WO_EXT"
+    ssh-add /tmp/$SSH_ENC_FILE_NAME_WO_EXT
+    echo "done preparing SSH-deployment-phase"
+fi
