8c897a6be72938cd8e593e0c29cfa31ec1e06242
Gerald <gu_falcon@hotmail.com>
2021-04-26 11:13:33 +0200

    Update script.sh

--- Java/script.sh
+++ Java/script.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
+mvn -B versions:set -DnewVersion=$POM_VERSION -DgenerateBackupPoms=false
 if tr_isSetAndNotFalse MAVEN_CENTRAL; then
   mvn clean deploy --settings $TRAVIS/settings.xml -B -U -DreleaseSonatype=true
 else
   mvn clean install --settings $TRAVIS/settings.xml -B -U -Prelease
 fi
-
 
