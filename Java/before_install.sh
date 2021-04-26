f376ef60e2df9767bf09adcc4e7e03de88f544f2
Gerald <gu_falcon@hotmail.com>
2021-04-26 11:23:48 +0200

    Update before_install.sh

--- Java/before_install.sh
+++ Java/before_install.sh
@@ -13,11 +13,13 @@ echo "Extracting project data from POM"
 export LOCAL_REPO=$(mvn -q -Dexec.executable=echo -Dexec.args='${settings.localRepository}' --non-recursive exec:exec)
 export GROUP_ID=$(mvn -q -Dexec.executable=echo -Dexec.args='${project.groupId}' --non-recursive exec:exec)
 export ARTIFACT_ID=$(mvn -q -Dexec.executable=echo -Dexec.args='${project.artifactId}' --non-recursive exec:exec)
-export POM_VERSION=$(mvn -q -Dexec.executable=echo -Dexec.args='${project.version}' --non-recursive exec:exec)
+export POM_VERSION_LOCAL=$(mvn -q -Dexec.executable=echo -Dexec.args='${project.version}' --non-recursive exec:exec)
+export POM_VERSION=$(git describe --tags)
 
 echo "LOCAL_REPO=${LOCAL_REPO}"
 echo "GROUP_ID=${GROUP_ID}"
 echo "ARTIFACT_ID=${ARTIFACT_ID}"
+echo "POM_VERSION_LOCAL=${POM_VERSION_LOCAL}"
 echo "POM_VERSION=${POM_VERSION}"
 
 if tr_isSetAndNotFalse DEPLOY; then
